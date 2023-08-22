5ec67998ea602454d2ad9cf931fd4ac4a6965080
Tashrik Anam <anampartho31@gmail.com>
2023-08-22 22:43:41 +0600

    Add product in cart ui

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -4,13 +4,14 @@ import Link from "next/link";
 interface NavItem {
   href: string;
   children: ReactNode;
+  className?: string;
 }
 
-const NavItem = ({ href, children }: NavItem) => {
+const NavItem = ({ href, children, className }: NavItem) => {
   return (
     <Link
       href={href}
-      className="px-1 mx-6 block text-xl font-bold text-green-950"
+      className={`px-1 mx-6 block text-xl font-bold text-green-950 ${className}`}
     >
       {children}
     </Link>
--- src/components/UserIcons/UserIcons.tsx
+++ src/components/UserIcons/UserIcons.tsx
@@ -1,8 +1,13 @@
+"use client";
 import { LuUser, LuShoppingCart, LuSearch } from "react-icons/lu";
 import NavItem from "../Navbar/NavItem";
 import { ClassName } from "@/utils/interfaces";
+import { useMiniSelector } from "@/redux/store";
 
 const UserIcons = ({ className }: ClassName) => {
+  const productsInCart = useMiniSelector(
+    (state) => state.cartReducer.value.products.length
+  );
   return (
     <ul className={`flex items-center ${className} text-green-800`}>
       <li>
@@ -16,8 +21,15 @@ const UserIcons = ({ className }: ClassName) => {
         </NavItem>
       </li>
       <li>
-        <NavItem href="/cart">
+        <NavItem href="/cart" className="relative">
           <LuShoppingCart />
+          {productsInCart ? (
+            <span className="font-thin w-6 h-6 rounded-full bg-green-800 text-white text-[50%] flex items-center justify-center absolute top-[-50%] right-[-50%]">
+              {productsInCart}
+            </span>
+          ) : (
+            ""
+          )}
         </NavItem>
       </li>
     </ul>
