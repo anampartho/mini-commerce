a2238b3372e8eec8bd020e47156595387bddebc1
Tashrik Anam <anampartho31@gmail.com>
2023-08-22 18:24:03 +0600

    Remove custom font

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,28 +2,6 @@ import { ClassName } from "@/utils/interfaces";
 import Link from "next/link";
 import React from "react";
 
-import localFont from "next/font/local";
-
-const ostrich = localFont({
-  src: [
-    {
-      path: "../../../public/fonts/OstrichSans-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../../public/fonts/OstrichSans-Bold.woff2",
-      weight: "bold",
-      style: "normal",
-    },
-    {
-      path: "../../../public/fonts/OstrichSans-Heavy.woff2",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-});
-
 const Logo = ({ className }: ClassName) => {
   return (
     <Link href="/" className={`text-5xl font-bold ${className}`}>
