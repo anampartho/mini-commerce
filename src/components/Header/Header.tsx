6c7bc2bf9de3d5f686bbcff6b88d94babdcf4a93
Tashrik Anam <anampartho31@gmail.com>
2023-08-22 21:35:02 +0600

    Comment out unused import

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,13 @@
-"use client";
 import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
 import Logo from "@/components/Logo/Logo";
 import UserIcons from "@/components/UserIcons/UserIcons";
-import { add, remove } from "@/redux/features/cart-slice";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "@/redux/store";
+// import { add, remove } from "@/redux/features/cart-slice";
+// import { useDispatch } from "react-redux";
+// import { AppDispatch } from "@/redux/store";
 
 const Header = () => {
-  const dispatch = useDispatch<AppDispatch>();
+  // const dispatch = useDispatch<AppDispatch>();
 
   return (
     <header className="px-10 py-5 ">
