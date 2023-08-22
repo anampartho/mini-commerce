f52a1e622371eab8a1dd364939c88bf1a1204c36
Tashrik Anam <anampartho31@gmail.com>
2023-08-22 14:14:33 +0600

    Add UserIcons component

--- src/components/UserIcons/UserIcons.tsx
+++ src/components/UserIcons/UserIcons.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { LuUser, LuShoppingCart, LuSearch } from "react-icons/lu";
+import NavItem from "../Navbar/NavItem";
 
 const UserIcons = () => {
-  return <div>UserIcons</div>;
+  return (
+    <ul className="flex items-center">
+      <li>
+        <NavItem href="">
+          <LuSearch />
+        </NavItem>
+      </li>
+      <li>
+        <NavItem href="">
+          <LuUser />
+        </NavItem>
+      </li>
+      <li>
+        <NavItem href="/cart">
+          <LuShoppingCart />
+        </NavItem>
+      </li>
+    </ul>
+  );
 };
 
 export default UserIcons;
