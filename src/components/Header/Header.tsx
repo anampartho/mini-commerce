2f6e97c305983651d95f729f30210aebfa92c833
Tashrik Anam <anampartho31@gmail.com>
2023-08-23 06:43:47 +0600

    Fix build error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Logo from "@/components/Logo/Logo";
 import UserIcons from "@/components/UserIcons/UserIcons";
 import SidebarToggle from "../SidebarToggle/SidebarToggle";
 
-const Header = ({ className }: { className: string }) => {
+const Header = ({ className }: { className?: string }) => {
   return (
     <header className={`px-10 py-5 ${className}`}>
       <div className="max-w-screen-2xl mx-auto flex flex-wrap justify-between items-center">
